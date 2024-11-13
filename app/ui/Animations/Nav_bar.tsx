1ac93a02a51e1dfd6694f608fa81458b454e1fb7
Jangidyogesh12 <107015666+Jangidyogesh12@users.noreply.github.com>
2024-11-13 19:18:02 +0530

    Improving nav bar

--- app/ui/Animations/Nav_bar.tsx
+++ app/ui/Animations/Nav_bar.tsx
@@ -31,8 +31,8 @@ const Nav_bar:React.FC=()=>{
     },[]);
 
     return (
-        <div className={`flex flex-row-reverse h-12 items-center transition-transform w-full duration-[2000ms] transform ${navSlider ? 'translate-y-0' : '-translate-y-12'} fixed z-20 ${isScrolled ? 'bg-[#000000a3] text-white':'bg-[rgba(0,0,0,0.26)] text-black'}`}>
-            <h1 className={`${roboto_slab.className} pr-20 cursor-pointer`}onClick={()=>Scroll('Footer')}>Get in Touch</h1> 
+        <div className={`flex flex-row-reverse h-12 items-center justify-center md:justify-start transition-transform w-full duration-[2000ms] transform ${navSlider ? 'translate-y-0' : '-translate-y-12'} fixed z-20 ${isScrolled ? 'bg-[#000000a3] text-white':'bg-[rgba(0,0,0,0.26)] text-black'}`}>
+            <h1 className={`${roboto_slab.className} pr-0 md:pr-20 cursor-pointer`}onClick={()=>Scroll('Footer')}>Get in Touch</h1> 
             <h1 className={`${roboto_slab.className} pr-7 cursor-pointer`} onClick={()=>Scroll('About')}>About</h1>
             <h1 className={`${roboto_slab.className} pr-7 cursor-pointer `} onClick={()=>Scroll('MyWork')}>Projects</h1>
             <h1 className={`${roboto_slab.className} pr-7 cursor-pointer `} onClick={()=>Scroll('Home')}>Home</h1>
